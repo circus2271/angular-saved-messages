20d4994bd7c1a03201869203766afb720f88ea97
rodionov80 <rodionov134@gmail.com>
2020-11-02 04:31:42 +0300

    Add validation

--- src/app/create-message/create-message.component.ts
+++ src/app/create-message/create-message.component.ts
@@ -1,18 +1,32 @@
-import { Component } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
-import { MessageService} from "../shared/message.service";
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { MessageService } from "../shared/message.service";
 
 @Component({
   selector: 'app-create-message',
   templateUrl: './create-message.component.html',
   styleUrls: ['./create-message.component.scss']
 })
-export class CreateMessageComponent {
-  newMessageForm = new FormGroup({
-    username: new FormControl(''),
-    message: new FormControl('')
-  })
+export class CreateMessageComponent implements OnInit {
+  newMessageForm: FormGroup;
+
   constructor(private messageService: MessageService) { }
+
+  ngOnInit() {
+    this.newMessageForm = new FormGroup({
+      username: new FormControl('', [
+        Validators.required,
+        Validators.minLength(1)
+      ]),
+      message: new FormControl('', [
+        Validators.required,
+        Validators.minLength(1)
+      ])
+    })
+  }
+
+  get username() { return this.newMessageForm.get('username'); }
+  get message() { return this.newMessageForm.get('message'); }
 
   onSubmit():void {
     const {username, message} = this.newMessageForm.controls;
